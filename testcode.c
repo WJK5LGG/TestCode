daacb4c25e3f6354e5a242a9c5fc8b12eded77c4
WJK5LGG <149886814+WJK5LGG@users.noreply.github.com>
2026-08-16 19:11:42 +0800

    Update testcode.c
    程序编码注释第一次打印信息

--- testcode.c
+++ testcode.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    printf("hello world\n");
+    // printf("hello world\n");
     printf("hello everyone\n");
     printf("add print message\n");
     printf("add print message again\n");
